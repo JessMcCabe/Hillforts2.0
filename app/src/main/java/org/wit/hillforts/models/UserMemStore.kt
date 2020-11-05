af76656bcaf4faed303fd297a61a762daa8e95d9
Jessica McCabe <mccabejessica88@gmail.com>
2020-11-05 19:41:11 +0000

    Basic Login & SignUp
    User can sign up and user is written to JSON file. User can log in but is not yet validated against user details in JSON file.

---
+++ app/src/main/java/org/wit/hillforts/models/UserMemStore.kt
@@ -0,0 +1,47 @@
+package org.wit.hillforts.models
+
+
+
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+var lastUserId =0L
+
+internal fun getUserId(): Long{
+    return lastUserId++
+}
+
+class UserMemStore : UserStore, AnkoLogger {
+
+    val users = ArrayList<UserModel>()
+
+    override fun findAll(): List<UserModel> {
+        return users
+    }
+
+    override fun create(user: UserModel) {
+        users.add(user)
+        logAll();
+    }
+
+
+    override fun update(user: UserModel) {
+        var foundUser: UserModel? = users.find { u -> u.id == user.id }
+        if (foundUser != null) {
+            foundUser.email = user.email
+            foundUser.password = user.password
+
+            logAll()
+        }
+    }
+
+    override fun delete(user: UserModel) {
+        var foundUser: UserModel? = users.find { u -> u.id == user.id }
+        users.remove(foundUser)
+        logAll()
+    }
+
+    fun logAll(){
+        users.forEach{info("${it}")}
+    }
+}
