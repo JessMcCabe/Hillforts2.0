a68f2662e6ebae7011c70ba0fd5bd222e57da7f3
Jessica McCabe <mccabejessica88@gmail.com>
2020-11-07 16:08:54 +0000

    Check Box for Visisted
    If the hillfort has been visited then the box can be checked. The state saved state of visited or not visited is shown by the check box

--- app/src/main/java/org/wit/hillforts/models/HillfortModel.kt
+++ app/src/main/java/org/wit/hillforts/models/HillfortModel.kt
@@ -6,6 +6,7 @@ import kotlinx.android.parcel.Parcelize
 @Parcelize
 data class HillfortModel(var id: Long = 0,
                          var userId: Long = 0,
+                         var visited: Boolean = false,
                          var title: String = "",
                          var description: String ="",
                          var image1: String = "",
