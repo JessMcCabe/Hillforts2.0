ff7cc91959f2ebc0507b00a289a6f13ed5acc667
Jessica McCabe <mccabejessica88@gmail.com>
2020-12-23 09:44:28 +0000

    Initial Login
    Initial Login and sign up with firebase, does not yet bring user into app

--- app/src/main/java/org/wit/hillforts/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/hillforts/activities/LoginActivity.kt
@@ -1,114 +1,220 @@
 package org.wit.hillforts.activities
 
 import android.os.Bundle
-import android.util.Patterns
+import android.text.TextUtils
+import android.util.Log
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
+import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.intentFor
-import org.jetbrains.anko.startActivityForResult
+import org.jetbrains.anko.startActivity
 import org.wit.hillforts.R
-import org.wit.hillforts.main.MainApp
-import org.wit.hillforts.models.UserModel
+import org.wit.hillforts.createLoader
+import org.wit.hillforts.hideLoader
+import org.wit.hillforts.showLoader
 import org.wit.hillforts.views.hillfortlist.HillfortListView
 
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var app : MainApp
-    lateinit var etEmail: EditText
-    lateinit var  etPassword: EditText
-    val MIN_PASSWORD_LENGTH = 6
-    var user = UserModel()
+//import com.google.firebase.quickstart.auth.R
+
+
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
     val USER_REQUEST = 4
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+    lateinit var loader : AlertDialog
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        app = application as MainApp
-        viewInitializations()
+
+        // Buttons
+        emailSignInButton.setOnClickListener(this)
+        emailCreateAccountButton.setOnClickListener(this)
+        signOutButton.setOnClickListener(this)
+        verifyEmailButton.setOnClickListener(this)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
+
+        loader = createLoader(this)
     }
 
-    private fun viewInitializations() {
-        etEmail = findViewById(R.id.et_email)
-        etPassword = findViewById(R.id.et_password)
-
-
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
+    // [END on_start_check_user]
 
-    // Checking if the input in form is valid
-    private fun validateInput(): Boolean {
-        if (etEmail.text.toString() == "") {
-            etEmail.error = "Please Enter Email"
-            return false
+    private fun createAccount(email: String, password: String) {
+        Log.d(TAG, "createAccount:$email")
+        if (!validateForm()) {
+            return
         }
-        if (etPassword.text.toString() == "") {
-            etPassword.error = "Please Enter Password"
-            return false
-        }
 
-        // checking the proper email format
-        if (!isEmailValid(etEmail.text.toString())) {
-            etEmail.error = "Please Enter Valid Email"
-            return false
-        }
+        //showLoader(loader, "Creating Account...")
 
-        // checking minimum password Length
-        if (etPassword.text.length < MIN_PASSWORD_LENGTH) {
-            etPassword.error = "Password Length must be more than " + MIN_PASSWORD_LENGTH + "characters"
-            return false
-        }
-        return true
-    }
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
 
-    private fun isEmailValid(email: String?): Boolean {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-
-    private fun isExistingUser(): Boolean{
-        getUserFromFile()
-        //Check if a user with that email exists
-        return true
-
-    }
-
-    private fun isCorrectPassword(): Boolean{
-        val returnedUser = getUserFromFile()
-        var success = false
-        if(returnedUser != null){
-            if(returnedUser.password == etPassword!!.text.toString() )
-            {
-                success = true
+                // [START_EXCLUDE]
+                //hideLoader(loader)
+                // [END_EXCLUDE]
             }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(email: String, password: String) {
+        Log.d(TAG, "signIn:$email")
+        if (!validateForm()) {
+            return
         }
 
+       // showLoader(loader, "Logging In...")
 
-            return success
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
 
-
+                // [START_EXCLUDE]
+                if (!task.isSuccessful) {
+                    status.setText(R.string.auth_failed)
+                }
+                hideLoader(loader)
+                // [END_EXCLUDE]
+            }
+        // [END sign_in_with_email]
     }
 
-    private fun getUserFromFile(): UserModel{
-
-        user = app.users.findOne(etEmail!!.text.toString())!!
-
-        return user
+    private fun signOut() {
+        auth.signOut()
+        updateUI(null)
     }
 
-    fun performSignIn(v: View) {
-        if (validateInput() && isExistingUser() && isCorrectPassword()) {
+    private fun sendEmailVerification() {
+        // Disable button
+        verifyEmailButton.isEnabled = false
 
+        // Send verification email
+        // [START send_email_verification]
+        val user = auth.currentUser
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(this) { task ->
+                // [START_EXCLUDE]
+                // Re-enable button
+                verifyEmailButton.isEnabled = true
 
-            user = app.users.findOne(etEmail!!.text.toString())!!
-            Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show()
-            startActivityForResult (intentFor<HillfortListView>().putExtra("user", user), USER_REQUEST)
-
-        }else
-        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Log.e(TAG, "sendEmailVerification", task.exception)
+                    Toast.makeText(baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+                // [END_EXCLUDE]
+            }
+        // [END send_email_verification]
     }
 
-    fun goToSignup(v: View) {
-        startActivityForResult<SignupActivity>(0)
+    private fun validateForm(): Boolean {
+        var valid = true
+
+        val email = fieldEmail.text.toString()
+        if (TextUtils.isEmpty(email)) {
+            fieldEmail.error = "Required."
+            valid = false
+        } else {
+            fieldEmail.error = null
+        }
+
+        val password = fieldPassword.text.toString()
+        if (TextUtils.isEmpty(password)) {
+            fieldPassword.error = "Required."
+            valid = false
+        } else {
+            fieldPassword.error = null
+        }
+
+        return valid
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        hideLoader(loader)
+        if (user != null) {
+            status.text = getString(R.string.emailpassword_status_fmt,
+                user.email, user.isEmailVerified)
+            detail.text = getString(R.string.firebase_status_fmt, user.uid)
+
+            emailPasswordButtons.visibility = View.GONE
+            emailPasswordFields.visibility = View.GONE
+            signedInButtons.visibility = View.VISIBLE
+
+            verifyEmailButton.isEnabled = !user.isEmailVerified
+            //startActivityForResult (intentFor<HillfortListView>().putExtra("user", user), USER_REQUEST)
+        } else {
+            status.setText(R.string.signed_out)
+            detail.text = null
+
+            emailPasswordButtons.visibility = View.VISIBLE
+            emailPasswordFields.visibility = View.VISIBLE
+            signedInButtons.visibility = View.GONE
+        }
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.emailCreateAccountButton -> createAccount(fieldEmail.text.toString(), fieldPassword.text.toString())
+            R.id.emailSignInButton -> signIn(fieldEmail.text.toString(), fieldPassword.text.toString())
+            R.id.signOutButton -> signOut()
+            R.id.verifyEmailButton -> sendEmailVerification()
+        }
+    }
+
+    companion object {
+        private const val TAG = "EmailPassword"
     }
 }
